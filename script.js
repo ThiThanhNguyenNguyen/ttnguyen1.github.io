bc6e31fbd0d1e3818e74ebb92cca055c6fe8ceee
ThiThanhNguyenNguyen <nguyenntt89@gmail.com>
2020-08-03 22:57:17 -0400

    upload pages

---
+++ script.js
@@ -0,0 +1,6 @@
+/* Add any JavaScript you need to this file. */
+window.onload = function() {
+  document.title = 'NguyenTea Website';
+  document.querySelector('#foot_').innerHTML =
+    '&copy; 2020, N & Tea. All rights reserved. Developed by Thi Thanh Nguyen Nguyen.';
+};
